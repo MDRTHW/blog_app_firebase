c73d2c8b88de52fd40d174e87c4fb3ee1994e945
MDRTHW <md3rahat2cse93@gmail.com>
2023-02-01 00:28:26 +0600

    homescreen view updated

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:blog_app_firebase/screens/add_post.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -9,26 +11,80 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final databaseReference = FirebaseDatabase.instance.ref().child('Posts');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff9fafc),
       appBar: AppBar(
         automaticallyImplyLeading: false,
         title: Text('New Blogs'),
         // centerTitle: true,
         actions: [
           InkWell(
-              onTap: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => AddPostScreen()));
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => AddPostScreen()));
               },
               child: Icon(Icons.add)),
-          SizedBox(width: 20,),
+          SizedBox(
+            width: 20,
+          ),
         ],
       ),
-      body: Column(
-        children: [
-
-        ],
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0,vertical: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: FirebaseAnimatedList(
+                query: databaseReference.child('Post List'),
+                itemBuilder: (BuildContext context, DataSnapshot snapshot,
+                    Animation<double> animation, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.grey.shade100,
+                        borderRadius: BorderRadius.circular(10)
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: FadeInImage.assetNetwork(
+                              fit: BoxFit.cover,
+                              width: MediaQuery.of(context).size.width * 1,
+                              height: MediaQuery.of(context).size.height * .25,
+                              placeholder: 'images/tohru.jpg',
+                              image: snapshot.child('pImage').value!.toString(),
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(snapshot.child('pTitle').value!.toString(), style: TextStyle(fontSize: 23,fontWeight: FontWeight.w500),),
+                          ),
+                          SizedBox(height: 10,),
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(snapshot.child('pDescription').value!.toString(), style: TextStyle(fontSize: 16,fontWeight: FontWeight.w400),),
+                          ),
+                          SizedBox(height: 10,),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
